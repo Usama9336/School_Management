c0bd7d3d17a5a34a5f05139fe14dc9ec3b262465
Usama Arshad <109479121+Usama9336@users.noreply.github.com>
2024-11-30 14:59:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(json());
 app.use('/api', schoolRoutes);
-app.get('/',(req,res)=>{
-res.status(200).send('Landing Page');
-});
 app.listen(PORT, () => {
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
