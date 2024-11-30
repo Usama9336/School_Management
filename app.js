e33ace4fa856e27d43eba73e4591ef9684d11086
Usama Arshad <109479121+Usama9336@users.noreply.github.com>
2024-11-30 20:38:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 import express from 'express';
 import bodyParser from 'body-parser';
+import pool from './config/database.js';
+import { initializeTables } from './config/database.js';
 
 const { json } = bodyParser;
 import schoolRoutes from './routes/schoolRoutes.js';
@@ -10,6 +12,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(json());
 app.use('/api', schoolRoutes);
-app.listen(PORT, () => {
-    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+
+
+async function startServer() {
+    try {
+      // Initialize tables before starting the server
+      await initializeTables();
+  
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error('Server startup failed:', error);
+      process.exit(1);
+    }
+  }
+
+
+  startServer();
